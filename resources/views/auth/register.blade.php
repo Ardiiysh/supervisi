b72254652f877ca79b87794a4e970b0161b4b9a7
ardiyansahh24 <ardiyansah4316@gmail.com>
2020-09-18 09:54:53 +0700

    modified

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,7 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" type="text/css" href="{!! asset('assets/css/login.css') !!}">
 
 @extends('layouts.app')
 
